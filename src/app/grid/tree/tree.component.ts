b833ed2436fef4f93ddb6e37559172936a665567
Ashik gowda <ashikssgowda644@gmail.com>
2019-05-05 19:08:43 +0530

    [PATCH] Added addTextarea method

--- src/app/grid/tree/tree.component.ts
+++ src/app/grid/tree/tree.component.ts
@@ -11,7 +11,21 @@ export class TreeComponent implements OnInit {
 
   ngOnInit() {
   }
- 
+
+ chapterList:any = [];
+ sectionList:any = [];
+
+  addChapter(){        
+      this.chapterList.push('text_area'+ (this.chapterList.length + 1));
+  }
+
+  addSection(){        
+    this.chapterList.push('text_area'+ (this.chapterList.length + 1));
+}
+  removeTextArea(index){
+      this.chapterList.splice(index, 1);
+  }
+
 }
 
 
