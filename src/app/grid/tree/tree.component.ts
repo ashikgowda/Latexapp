7d5836fa900a423b0365ea6cd0265a2b4e7cbb11
Arun Jois <arun.joiss@gmail.com>
2019-05-07 23:14:24 +0530

    Grid is Working

--- src/app/grid/tree/tree.component.ts
+++ src/app/grid/tree/tree.component.ts
@@ -1,8 +1,8 @@
-import {FlatTreeControl} from '@angular/cdk/tree';
-import {Component, Injectable} from '@angular/core';
-import {MatTreeModule} from '@angular/material/tree';
-import {MatTreeFlatDataSource, MatTreeFlattener} from '@angular/material/tree';
-import {BehaviorSubject, Observable, of as observableOf} from 'rxjs';
+import { FlatTreeControl } from '@angular/cdk/tree';
+import { Component, Injectable } from '@angular/core';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatTreeFlatDataSource, MatTreeFlattener } from '@angular/material/tree';
+import { BehaviorSubject, Observable, of as observableOf } from 'rxjs';
 
 /**
  * File node data with nested structure.
@@ -17,37 +17,17 @@ export class FileNode {
 /** Flat node with expandable and level information */
 export class FileFlatNode {
   constructor(
-    public expandable: boolean, public filename: string, public level: number, public type: any) {}
+    public expandable: boolean, public filename: string, public level: number, public type: any) { }
 }
 
 /**
  * The file structure tree data in string. The data could be parsed into a Json object
  */
 const TREE_DATA = JSON.stringify(
-{
-	"name":"Sample Latex",
-	"version":"0.0.1",
-	"chapter 1":{
-		"name":"Basic",
-		"section":{
-			"name":"Section Type",
-			"subsection 1.1":{
-				"heading":"Section Type",
-				"text 1":"The Basic Section in a book are chapter,section, subsection and so on. These section will appear in the ",
-				"underline":"table of contents",
-				"text 2":"of the book",
-			},
-			"subsection 1.2":{
-				"heading":"Example Subsection",
-				"text":"This is just one exaple for a subsection.",
-			},
-			"subsection 1.3":{
-				"heading":"Basic Page Setting",
-				"text":"In order to show how the basic page setting are set up, we will use a long dummy text with the package ",
-			}
-		}
-	}
-}
+  {
+    "Introduction": "1. INTRODUCTION",
+    "SECTION": "SOME TEXT"
+  }
 );
 
 /**
