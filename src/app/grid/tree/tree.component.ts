b4261e93c7a5cee2e76404d8f52c0e453ce353cd
Ashik gowda <ashikssgowda644@gmail.com>
2019-05-14 00:28:50 +0530

    [PATCH]Merged tool component

--- src/app/grid/tree/tree.component.ts
+++ src/app/grid/tree/tree.component.ts
@@ -8,30 +8,30 @@ import { BehaviorSubject, Observable, of as observableOf } from 'rxjs';
  * File node data with nested structure.
  * Each node has a filename, and a type or a list of children.
  */
-export class FileNode {
-  children: FileNode[];
-  filename: string;
-  type: any;
-}
+// export class FileNode {
+//   children: FileNode[];
+//   filename: string;
+//   type: any;
+// }
 
 /** Flat node with expandable and level information */
-export class FileFlatNode {
-  constructor(
-    public expandable: boolean, public filename: string, public level: number, public type: any) { }
-}
+// export class FileFlatNode {
+//   constructor(
+//     public expandable: boolean, public filename: string, public level: number, public type: any) { }
+// }
 
 /**
  * The file structure tree data in string. The data could be parsed into a Json object
  */
-const TREE_DATA = JSON.stringify(
-  {
-    "Introduction": "1. INTRODUCTION",
-    "1.1 Basic": { 
-      "PARAGRAPH ONE": "The learning algorithm plays an important role in pattern classification particularlyif the image object varies its",
-      "PATAGRAPH TWO": "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem  Ipsum  has  been  the  industry’s  standard  dummy  text  ever  since  the1500s,  when  an  unknown  printer  took  a  galley  of  type  and  scrambled  it  tomake a type specimen book.  It has survived not only five centuries, but alsothe  leap  into  electronic  typesetting,  remaining  essentially  unchanged.   It  waspopularised in the 1960s with the release of Letraset sheets containing LoremIpsum passages, and more recently with desktop publishing software like AldusPageMaker including versions of Lorem Ipsum.",
-    }
-  }
-);
+// const TREE_DATA = JSON.stringify(
+//   {
+//     "Introduction": "1. INTRODUCTION",
+//     "1.1 Basic": { 
+//       "PARAGRAPH ONE": "The learning algorithm plays an important role in pattern classification particularlyif the image object varies its",
+//       "PATAGRAPH TWO": "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem  Ipsum  has  been  the  industry’s  standard  dummy  text  ever  since  the1500s,  when  an  unknown  printer  took  a  galley  of  type  and  scrambled  it  tomake a type specimen book.  It has survived not only five centuries, but alsothe  leap  into  electronic  typesetting,  remaining  essentially  unchanged.   It  waspopularised in the 1960s with the release of Letraset sheets containing LoremIpsum passages, and more recently with desktop publishing software like AldusPageMaker including versions of Lorem Ipsum.",
+//     }
+//   }
+// );
 
 /**
  * File database, it can build a tree structured Json object from string.
@@ -40,83 +40,89 @@ const TREE_DATA = JSON.stringify(
  * The input will be a json object string, and the output is a list of `FileNode` with nested
  * structure.
  */
-@Injectable()
-export class FileDatabase {
-  dataChange = new BehaviorSubject<FileNode[]>([]);
+// @Injectable()
+// export class FileDatabase {
+//   dataChange = new BehaviorSubject<FileNode[]>([]);
 
-  get data(): FileNode[] { return this.dataChange.value; }
+//   get data(): FileNode[] { return this.dataChange.value; }
 
-  constructor() {
-    this.initialize();
-  }
+//   constructor() {
+//     // this.initialize();
+//   }
 
-  initialize() {
-    // Parse the string to json object.
-    const dataObject = JSON.parse(TREE_DATA);
+  // initialize() {
+  //   // Parse the string to json object.
+  //   const dataObject = JSON.parse(TREE_DATA);
 
-    // Build the tree nodes from Json object. The result is a list of `FileNode` with nested
-    //     file node as children.
-    const data = this.buildFileTree(dataObject, 0);
+  //   // Build the tree nodes from Json object. The result is a list of `FileNode` with nested
+  //   //     file node as children.
+  //   const data = this.buildFileTree(dataObject, 0);
 
-    // Notify the change.
-    this.dataChange.next(data);
-  }
+  //   // Notify the change.
+  //   this.dataChange.next(data);
+  // }
 
   /**
    * Build the file structure tree. The `value` is the Json object, or a sub-tree of a Json object.
    * The return value is the list of `FileNode`.
    */
-  buildFileTree(obj: object, level: number): FileNode[] {
-    return Object.keys(obj).reduce<FileNode[]>((accumulator, key) => {
-      const value = obj[key];
-      const node = new FileNode();
-      node.filename = key;
+//   buildFileTree(obj: object, level: number): FileNode[] {
+//     return Object.keys(obj).reduce<FileNode[]>((accumulator, key) => {
+//       const value = obj[key];
+//       const node = new FileNode();
+//       node.filename = key;
 
-      if (value != null) {
-        if (typeof value === 'object') {
-          node.children = this.buildFileTree(value, level + 1);
-        } else {
-          node.type = value;
-        }
-      }
+//       if (value != null) {
+//         if (typeof value === 'object') {
+//           node.children = this.buildFileTree(value, level + 1);
+//         } else {
+//           node.type = value;
+//         }
+//       }
 
-      return accumulator.concat(node);
-    }, []);
-  }
-}
+//       return accumulator.concat(node);
+//     }, []);
+//   }
+// }
 
-/**
- * @title Tree with flat nodes
- */
+
 @Component({
   selector: 'app-tree',
   templateUrl: 'tree.component.html',
   styleUrls: ['tree.component.css'],
-  providers: [FileDatabase]
+  // providers: [FileDatabase]
 })
 export class TreeComponent {
-  treeControl: FlatTreeControl<FileFlatNode>;
-  treeFlattener: MatTreeFlattener<FileNode, FileFlatNode>;
-  dataSource: MatTreeFlatDataSource<FileNode, FileFlatNode>;
 
-  constructor(database: FileDatabase) {
-    this.treeFlattener = new MatTreeFlattener(this.transformer, this._getLevel,
-      this._isExpandable, this._getChildren);
-    this.treeControl = new FlatTreeControl<FileFlatNode>(this._getLevel, this._isExpandable);
-    this.dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
+  public greeting="";
+  // treeControl: FlatTreeControl<FileFlatNode>;
+  // treeFlattener: MatTreeFlattener<FileNode, FileFlatNode>;
+  // dataSource: MatTreeFlatDataSource<FileNode, FileFlatNode>;
 
-    database.dataChange.subscribe(data => this.dataSource.data = data);
+  // constructor(database: FileDatabase) {
+  //   this.treeFlattener = new MatTreeFlattener(this.transformer, this._getLevel,
+  //     this._isExpandable, this._getChildren);
+  //   this.treeControl = new FlatTreeControl<FileFlatNode>(this._getLevel, this._isExpandable);
+  //   this.dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
+
+  //   database.dataChange.subscribe(data => this.dataSource.data = data);
+  // }
+
+  // transformer = (node: FileNode, level: number) => {
+  //   return new FileFlatNode(!!node.children, node.filename, level, node.type);
+  // }
+
+  // private _getLevel = (node: FileFlatNode) => node.level;
+
+  // private _isExpandable = (node: FileFlatNode) => node.expandable;
+
+  // private _getChildren = (node: FileNode): Observable<FileNode[]> => observableOf(node.children);
+
+  // hasChild = (_: number, _nodeData: FileFlatNode) => _nodeData.expandable;
+
+  public  addTextArea(event){
+    console.log(event);
+    this.greeting='welcome';}
   }
+    
 
-  transformer = (node: FileNode, level: number) => {
-    return new FileFlatNode(!!node.children, node.filename, level, node.type);
-  }
-
-  private _getLevel = (node: FileFlatNode) => node.level;
-
-  private _isExpandable = (node: FileFlatNode) => node.expandable;
-
-  private _getChildren = (node: FileNode): Observable<FileNode[]> => observableOf(node.children);
-
-  hasChild = (_: number, _nodeData: FileFlatNode) => _nodeData.expandable;
-}
